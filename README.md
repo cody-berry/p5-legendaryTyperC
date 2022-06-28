1c1e90c810bcd9f48f0e39df63af0d9fc6e66323
Cody <cody.tian@icloud.com>
2022-06-28 16:01:47 -0700

    reset project to template + coding plan

--- README.md
+++
@@ -1,2 +0,0 @@
-# p5-legendaryTyperC
-A typingclub.com clone where we use League of Legends champion data via the Riot API.
--- sketch.js
+++ sketch.js
@@ -1,48 +1,47 @@
 /**
- *  @author Cody
+ *  @author cody
  *  @date 2022.06.28
- *
- * 0. start with the template project
- * 1. console log a list of hero names from champion.json
- * 2. console log some info from a specific champion
- * 3. obtain a list of abilities from any hero
- * 4. obtain a list of lists of hero abilities
- * 5. make a typerc passage work with a champion.json 'blurb'
- * 6. fetch a few champion images
- * 7. fetch ability images from the multi-dimensional array
- *
- * 8. tinker with the items.json page
- * 9. display icon images
- *
- * draw diagram with rudimentary layout
- *
- * enable passage.js to handle multiple sections: 'nextSection()'. maybe new
- * object?
- *
- * filter tags out of abilities
- * color or format specific xml tags
+
+ make sure passage.js works first
+
+ 0. start with the template project
+ 1. console log a list of hero names from champion.json
+ 2. console log some info from a specific champion
+ 3. obtain a list of abilities from any hero
+ 4. obtain a list of lists of hero abilities
+ 5. make a typerc passage work with a champion.json 'blurb'
+ 6. fetch a few champion images
+ 7. fetch ability images from the multidimensional array
+
+ 8. tinker with the items.json page
+ 9. display icon images
+
+ draw diagram with rudimentary layout
+
+ enable passage.js to handle multiple sections: 'nextSection()' 🦔 Cody
+ suggests new object.
+
+ filter tags out of abilities
+ color or format specific xml tags
+
+
  */
 
 let font
 let instructions
 let debugCorner /* output debug text in the bottom left corner of the canvas */
-let correctSound /* the sound that we play when we get a correct key */
-let incorrectSound /* the sound that we play when we get an incorrect key */
-let passage
 
 
 function preload() {
     font = loadFont('data/consola.ttf')
-    correctSound = loadSound('data/correct.wav')
-    incorrectSound = loadSound('data/incorrect.wav')
 }
 
 
 function setup() {
-    let cnv = createCanvas(960, 480)
+    let cnv = createCanvas(600, 300)
     cnv.parent('#canvas')
     colorMode(HSB, 360, 100, 100, 100)
-    textFont(font, 23)
+    textFont(font, 14)
 
     /* initialize instruction div */
     instructions = select('#ins')
@@ -50,17 +49,11 @@ function setup() {
         numpad 1 → freeze sketch</pre>`)
 
     debugCorner = new CanvasDebugCorner(5)
-
-    passage = new Passage("this is a test" +
-        " passage\n\n\n\n\n\n\n\n\n\n\n\n\n\nscrolled?\n\n\n\n\n")
 }
 
 
 function draw() {
     background(234, 34, 24)
-    textFont(font, 23)
-
-    passage.show()
 
     /* debugCorner needs to be last so its z-index is highest */
     debugCorner.setText(`frameCount: ${frameCount}`, 2)
@@ -74,56 +67,11 @@ function draw() {
 
 function keyPressed() {
     /* stop sketch */
-    if (keyCode === 97) { // 97 is the keycode for numpad 1
+    if (keyCode === 97) { /* numpad 1 */
         noLoop()
         instructions.html(`<pre>
             sketch stopped</pre>`)
-    } if (keyCode === 98) { // 98 is the keycode for numpad 2
-        cardDataIndex -= 10
-        updateCard(cardDataIndex)
-    } if (keyCode === 100) { // 100 is the keycode for numpad 4
-        cardDataIndex--
-        updateCard(cardDataIndex)
-    } if (keyCode === 101) { // 101 is the keycode for numpad 5
-        cardDataIndex = round(random(0, cardData.length-1))
-        updateCard(cardDataIndex)
-    } if (keyCode === 102) { // 102 is the keycode for numpad 6
-        cardDataIndex++
-        updateCard(cardDataIndex)
-    } if (keyCode === 104) { // 104 is the keycode for numpad 8
-        cardDataIndex += 10
-        updateCard(cardDataIndex)
     }
-    /* a key has been typed! */
-    else {
-        // otherwise....
-        processKeyTyped(key)
-    }
-}
-
-// processes given key
-function processKeyTyped(key) {
-    // asterisk (*) = bullet point (•), and dash (-) = emdash (—). This is
-    // the key that needs to be typed in order to get the character correct.
-    let correctKey = passage.getCurrentChar(passage.index)
-    if (correctKey === "•") {
-        correctKey = "*"
-    } else if (correctKey === "—") {
-        correctKey = "-"
-    } if (passage.text.substring(passage.index, passage.index + 1) === "\n") {
-        correctKey = "Enter"
-    }
-    if (key !== "Shift" && key !== "Tab" && key !== "CapsLock" && key !== "Alt" && key !== "Control") {
-        if (key === correctKey) {
-            passage.setCorrect()
-            correctSound.play()
-        } else {
-            passage.setIncorrect()
-            incorrectSound.play()
-        }
-    }
-
-    print(key + "🆚" + correctKey + ", " + passage.index)
 }
 
 
